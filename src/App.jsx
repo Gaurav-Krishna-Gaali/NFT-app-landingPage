92153e174774d8f31d5bbf2d59a8050165512739
Gaurav-Krishna-Gaali <your_email_address@example.comcls>
2022-04-16 12:01:27 +0530

    1st View Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,25 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Download, Features, SectionWrapper } from "./components";
+import assets from "./assets";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <>
+      <SectionWrapper
+        title="Your Own store of Nifty NFTs, Start Selling & Growing"
+        description="Buy, store, collect NFTs, exchange & earn crypto. Join 25+ million people using ProNef Marketplace"
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+      {/* <SectionWrapper
+        title="Smart User Interface Marketplace"
+        description="e"
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      /> */}
+    </>
+  );
 }
 
-export default App
+export default App;
