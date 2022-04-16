ecd4a40edf5bc57a986f69b4859ff10af53e17b4
Gaurav-Krishna-Gaali <your_email_address@example.comcls>
2022-04-16 13:27:16 +0530

    Technology Section Check

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import assets from "../assets";
+import styles from "../styles/Global";
+
+const FeatureCard = ({ iconText, iconUrl }) => (
+  <div className={`${styles.featureCard} `}>
+    <img src={iconUrl} alt="icon" className={`${styles.featureImg}`} />
+    <p className={`${styles.featureText}`}>{iconText}</p>
+  </div>
+);
 
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <div className={`${styles.section} ${styles.bgPrimary} banner03`}>
+      <div className={`${styles.subSection} flex-col text-center`}>
+        <div className="">
+          <p className={`${styles.h1Text} ${styles.whiteText}`}>Technolgies</p>
+          <p className={`${styles.pText} ${styles.whiteText}`}>
+            ProNef has been developed using a cross-platform technology, React
+            Native.
+          </p>
+        </div>
+        <div className={`${styles.flexWrap}`}>
+          <FeatureCard iconUrl={assets.react} iconText="React Native" />
+          <FeatureCard iconUrl={assets.javascript} iconText="JavaScript" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Features;
